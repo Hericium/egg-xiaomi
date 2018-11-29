f871b89130a612ea5e8793b4bc02af3639daa0b9
beast <beast@beastdeMacBook-Air.local>
2018-11-29 15:27:00 +0800

    添加restful

--- app/router.js
+++ app/router.js
@@ -6,4 +6,9 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
+
+  // admin
+  router.resources('admin/managers', '/admin/managers', controller.admin.managers);
+  router.resources('admin/access', '/admin/access', controller.admin.access);
+  router.resources('admin/role', '/admin/role', controller.admin.role);
 };
