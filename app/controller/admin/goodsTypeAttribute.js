da51fd893934b0af02652d41a0b7a291bab752f6
qiyun <pxwpy53@gmail.com>
2019-01-09 14:06:10 +0800

    update

--- app/controller/admin/goodsTypeAttribute.js
+++ app/controller/admin/goodsTypeAttribute.js
@@ -2,25 +2,60 @@
 const BaseController = require('./base');
 class GoodsTypeAttributeController extends BaseController {
   async index() {
-    const doc = await this.ctx.model.GoodsTypeAttribute.find() || [];
+    const { _id } = this.ctx.request.query;
+    const doc = await this.ctx.model.GoodsTypeAttribute.aggregate([
+      {
+        $lookup: {
+          from: 'goodstypes',
+          localField: 'cate_id',
+          foreignField: '_id',
+          as: 'goods_type',
+        },
+      },
+      // 匹配到这个数据库下的和_id 相同的
+      {
+        $match: {
+          cate_id: this.app.mongoose.Types.ObjectId(_id),
+        },
+      },
+    ]);
+
     await this.ctx.render('/admin/goodsTypeAttribute/index', {
       doc,
-    }
-    );
+    });
   }
   async add() {
-    await this.ctx.render('/admin/goodsTypeAttribute/add');
+    const doc = await this.ctx.model.GoodsType.find() || [];
+    console.log('doc :', doc);
+    await this.ctx.render('/admin/goodsTypeAttribute/add', {
+      doc,
+    });
   }
   async doAdd() {
     const doc = this.ctx.request.body;
-    console.log('doc :', doc);
     await this.ctx.model.GoodsTypeAttribute(doc).save();
     await this.success('index');
   }
 
   async edit() {
     const { _id } = this.ctx.request.query;
-    const doc = await this.ctx.model.GoodsTypeAttribute.find({ _id });
+    const doc = await this.ctx.model.GoodsTypeAttribute.aggregate([
+      {
+        $lookup: {
+          from: 'goodstypes',
+          localField: 'cate_id',
+          foreignField: '_id',
+          as: 'goods_type',
+        },
+      },
+      // 匹配到这个数据库下的和_id 相同的
+      {
+        $match: {
+          _id: this.app.mongoose.Types.ObjectId(_id),
+        },
+      },
+    ]);
+    console.log('doc :', doc);
     await this.ctx.render('/admin/goodsTypeAttribute/edit', {
       doc,
     });
